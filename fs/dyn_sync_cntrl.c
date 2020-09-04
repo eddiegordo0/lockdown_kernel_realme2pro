7f00630a3ea1f20cd59f30eec7287a0b349f56a6
vijaymalav564 <jaymalav10@gmail.com>
2020-09-04 08:37:00 +0000

    Revert "fix compile error after enabling Dynamic fsync 2.0"
    This reverts commit 9b63ed62efa1f65e878d031f7662270417084f09.

--- fs/dyn_sync_cntrl.c
+++ fs/dyn_sync_cntrl.c
@@ -219,6 +219,7 @@ static int dyn_fsync_init(void)
 	}
 
 	lcd_notif.notifier_call = lcd_notifier_callback;
+	if (lcd_register_client(&lcd_notif) != 0) 
 	{
 		pr_err("%s: Failed to register lcd callback\n", __func__);
 
@@ -249,6 +250,7 @@ static void dyn_fsync_exit(void)
 	if (dyn_fsync_kobj != NULL)
 		kobject_put(dyn_fsync_kobj);
 	
+	lcd_unregister_client(&lcd_notif);
 		
 	pr_info("%s dynamic fsync unregistration complete\n", __FUNCTION__);
 }
